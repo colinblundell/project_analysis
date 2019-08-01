6e6002741c62340746da185e4a9c7f392f923ee9
Colin Blundell <blundell@chromium.org>
2019-08-01 13:46:40 +0200

    Update generated metadata

--- generate_metadata_for_list_of_commits.py
+++ generate_metadata_for_list_of_commits.py
@@ -8,7 +8,7 @@ import sys
 
 authors = {}
 reviewers = {}
-files_changed = []
+files_changed = set()
 
 # TODO: Pass via arg
 commits = open("test_commits.txt")
@@ -16,6 +16,9 @@ authors_file = open("/usr/local/google/home/blundell/identity_component_commit_a
 reviewers_file = open("/usr/local/google/home/blundell/identity_component_commit_reviewers.txt", "w")
 files_changed_file = open("/usr/local/google/home/blundell/identity_component_commit_files_changed.txt", "w")
 num_cls_with_reviews = 0
+total_lines_inserted = 0
+total_lines_deleted = 0
+
 for commit in commits:
   commit = commit[:-1]
   author = subprocess.check_output(["git", "show", "-s", "--format=\"%ae\"", commit],
@@ -26,7 +29,7 @@ for commit in commits:
     authors[author] = 0
   authors[author] += 1
 
-  commit_msg = subprocess.check_output(["git", "show", "--numstat"],
+  commit_msg = subprocess.check_output(["git", "show", "--numstat", commit],
                             cwd="/usr/local/google/home/blundell/clankium/src")
   cl_reviewed = False
   for line in commit_msg.splitlines():
@@ -41,14 +44,13 @@ for commit in commits:
         reviewers[email] = 0
       reviewers[email] += 1
     if len(line) and line[0].isdigit():
-      file_change_matcher = re.search(r"([0-9])*\t([0-9]*\)\t(.*)", line)
+      file_change_matcher = re.search(r"([0-9])*\t([0-9]*)\t(.*)", line)
       lines_inserted = file_change_matcher.group(1)
       lines_deleted = file_change_matcher.group(2)
-      file_changed = file_change_matcher.group(3)
-      print lines_inserted
-      print lines_deleted
-      print file_changed
-      files_changed.append(line)
+      changed_file = file_change_matcher.group(3)
+      total_lines_inserted += int(lines_inserted)
+      total_lines_deleted += int(lines_deleted)
+      files_changed.add(changed_file)
   if cl_reviewed:
     num_cls_with_reviews += 1
 
@@ -57,7 +59,9 @@ commits.close()
 metadata = {}
 metadata["authors"] = authors
 metadata["reviewers"] = reviewers
-metadata["files_changed"] = files_changed
+metadata["total_lines_inserted"] = total_lines_inserted
+metadata["total_lines_deleted"] = total_lines_deleted
+metadata["files_changed"] = list(files_changed)
 json.dump(metadata, sys.stdout)
 #print "Num CLs with reviews: " + str(num_cls_with_reviews)
 #pprint.pprint(authors, stream=authors_file)
